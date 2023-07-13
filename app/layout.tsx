e0a83f5a67d376e0577fbced1672937015d53978
Mariusz Wnęk <mariuszwnek@Mariuszs-MacBook-Pro.local>
2023-07-13 13:48:30 +0200

    Switch to Scroll Rig

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,20 @@
-import { Layout } from '@/components/dom/Layout'
-import '@/global.css'
+'use client' 
+import './globals.css'
+import { useRef, useState, useEffect } from 'react'
+import { useThree } from '@react-three/fiber'
+import { GlobalCanvas, ScrollScene, UseCanvas, SmoothScrollbar } from '@14islands/r3f-scroll-rig'
 
-export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
-}
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang='en' className='antialiased'>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html lang="en">
       <body>
-        {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
-        <Layout>{children}</Layout>
-      </body>
+      <GlobalCanvas />
+        {children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,18 @@
 'use client'
+import { SmoothScrollbar } from '@14islands/r3f-scroll-rig'
 
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
 
-const Logo = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
-const Dog = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
-const Duck = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Duck), { ssr: false })
-const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
-  ssr: false,
-  loading: () => (
-    <div>
-      <h1>Loading</h1>
-    </div>
-  ),
-})
-const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
-
-export default function Page() {
+export default function Home() {
   return (
-    <>
-      <div>
-        <View orbit className='viewSize'>
-          <Suspense fallback={null}>
-            <Dog scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
-            <Common color={'lightpink'} />
-          </Suspense>
-        </View>
-      </div>
-    </>
+
+<SmoothScrollbar>
+{(bind) => (
+      // Return a single DOM wrapper and spread all props {...bind}
+      <article {...bind}>This content will be smooth scrolled</article>
+    )}
+</SmoothScrollbar>
+
   )
 }
