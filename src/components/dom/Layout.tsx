8c29b15cbd893c742abeda07f6a640f1c8b9499e
Mariusz Wnęk <wnek.mariusz@gmail.com>
2023-07-13 09:28:00 +0200

    Fix styles

--- src/components/dom/Layout.tsx
+++ src/components/dom/Layout.tsx
@@ -8,16 +8,7 @@ const Layout = ({ children }) => {
   const ref = useRef()
 
   return (
-    <div
-      ref={ref}
-      style={{
-        position: 'relative',
-        width: ' 100%',
-        height: '100%',
-        overflow: 'auto',
-        touchAction: 'auto',
-      }}
-    >
+    <div className='dom' ref={ref}>
       {children}
       <Scene
         style={{
