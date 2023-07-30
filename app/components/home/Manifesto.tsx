fe258f0a784dbd10a71c7c605321186dc591d7c1
Mariusz Wnęk <wnek.mariusz@gmail.com>
2023-07-30 20:01:57 +0200

    Add styles to the manifesto

--- app/components/home/Manifesto.tsx
+++ app/components/home/Manifesto.tsx
@@ -2,37 +2,57 @@ export default function Manifesto() {
   return (
     <section className="manifesto">
       <div className="container">
-        <p>Manifesto</p>
-        <h2>
-          According to Harvard Business Review, 75% of startups unfortunately
-          don't succeed. I know the frustration of working hard and not seeing
-          results.
-        </h2>
-        <p>
+        <div className="manifesto_header">
+          <p className="label yellow">Manifesto</p>
+          <h2>
+            <span className="grey">
+              According to Harvard Business Review, 75% of startups
+              unfortunately don't succeed. I know the frustration of working
+              hard and not seeing results.
+            </span>{' '}
+            I also know the solution.
+          </h2>
+        </div>
+        <p className="manifesto_p1">
           Every new concept appears promising. It's easy to simply follow in the
           footsteps of competitors, trust your instincts, or optimistically hope
           for the best. However, our judgment is often clouded by subtle biases,
           leading to less than rational decision-making.
         </p>
-        <p>
+        <p className="manifesto_p2">
           Impactful design goes beyond just aesthetic appeal. It must directly
           contribute to business performance. My approach incorporates design,
           psychology, and technology, with a specific focus on driving product
           growth.
         </p>
-        <p>To that end, I vow to:</p>
-        <ol>
-          <li>Focus on the most critical performance metrics.</li>
-          <li>Identify and prioritize customer needs and expectations.</li>
-          <li>Create user experiences that drive conversions and growth.</li>
-          <li>Tell dad jokes on every occasion.</li>
-        </ol>
-
-        <h3>
-          Uncover what works for your business and how to grow your products by
-          design.
-        </h3>
-        <p>― Mariusz Wnęk</p>
+        <div className="manifesto_vows">
+          <p className="label">To that end, I vow to:</p>
+          <ol>
+            <li>
+              <span>01</span>Focus on the most critical performance metrics.
+            </li>
+            <li>
+              <span>02</span>Identify and prioritize customer needs and
+              expectations.
+            </li>
+            <li>
+              <span>03</span>Create user experiences that drive conversions and
+              growth.
+            </li>
+            <li>
+              <span>04</span>Tell dad jokes on every occasion.
+            </li>
+          </ol>
+        </div>
+        <div className="manifesto_cta">
+          <h2>
+            Uncover what works for your business and how to grow your products
+            by design.
+          </h2>
+          <p className="label">
+            <span className="yellow">―</span> Mariusz Wnęk
+          </p>
+        </div>
       </div>
     </section>
   );
